06d4ed64c2a9563a3b33b8ff18d2b7a6354f4501
Gospel-Columbus <132568918+Gospel-Columbus@users.noreply.github.com>
2025-07-24 02:09:15 +0100

    commit changes

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-//fetch each json item to the console.
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then((response) => response.json())
-  .then((json) => json.map((data) => {
-     console.log(data.title);
-     // fetch all the json items to the website.
-      const p = document.createElement("p");
-     p.textContent = `Body: ${data.body}, Id: ${data.id}, Title: ${data.title}, UserId: ${data.userId} `;
-    document.getElementById("content").appendChild(p);
+  .then((json) => {
+    const data = json;
+    console.log(data); // for accessing all the json data in the console.
 
-  }));
-    
-
-
-
-
+    // Iterate through each product and display all fields
+    data.forEach(item => {
+      const p = document.createElement('p');
+      p.textContent = `Body: ${data.body}, Id: ${data.id}, Title: ${data.title}, UserId: ${data.userId}`;
+      document.getElementById('content').appendChild(p);
+    });
+  });
